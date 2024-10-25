d1cd18b11456b6f74e40b6a83226f6edb4c1ca71
Dede Emiko <dede.emiko@gmail.com>
2024-10-24 21:55:50 -0400

    Import cors

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 import "dotenv/config";
 import express from "express";
-const app = express();
+import cors from "cors";
 
+
+const app = express();
 const PORT = process.env.PORT || 5050;
+
 
 import recipeRoutes from "./routes/recipe-routes.js"
 
+app.use(cors());
 
-app.use("/recipes", recipeRoutes);
+
+app.use("/api/recipes", recipeRoutes);
 
 app.listen(PORT, () => {
   console.log(`running at http://localhost:${PORT}`);
