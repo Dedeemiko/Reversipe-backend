8c3d0be8eefeebe91456fce6419cb9c5f143a469
Dede Emiko <dede.emiko@gmail.com>
2024-10-27 14:07:28 -0400

    Modify search by ingredients function to include recipes containing all ingredients searched for

--- controllers/recipe-controller.js
+++ controllers/recipe-controller.js
@@ -32,20 +32,49 @@ const searchByIngredients = async (req, res) => {
     const ingredientsArray = ingredients.split(",").map(item => item.trim());
 
     try {
-        const ingredientsFound = await knex("recipes")
-        .where(function() {
-            ingredientsArray.forEach((ingredient) => {
-                this.orWhere("ingredients", "like", `%${ingredient}%`);
+        const allIngredientsMatch = await knex("recipes")
+            .where(function() {
+                ingredientsArray.forEach((ingredient) => {
+                    this.andWhere("ingredients", "like", `%${ingredient}%`);
+                });
             });
-        });
-        res.status(200).json(ingredientsFound);
+
+        const allIngredientsIds = allIngredientsMatch.map(recipe => recipe.id);
+
+        const anyIngredientMatch = await knex("recipes")
+            .where(function() {
+                ingredientsArray.forEach((ingredient) => {
+                    this.orWhere("ingredients", "like", `%${ingredient}%`);
+                });
+            })
+            .andWhereNot("id", allIngredientsIds);
+
+        const results = [...allIngredientsMatch, ...anyIngredientMatch];       
+        res.status(200).json(results);
+
     } catch (err) {
         res.status(400).send(`Error searching for recipes by ingredients: ${err}`);
+    }
+};
+
+const getRecipeById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const recipe = await knex("recipes").where({ id }).first();
+
+        if (!recipe) {
+            return res.status(404).send("Recipe not found");
+        }
+
+        res.status(200).json(recipe);
+    } catch (err) {
+        res.status(500).send(`Error retrieving recipe by ID: ${err}`);
     }
 };
 
 export {
     index,
     searchByName,
-    searchByIngredients
+    searchByIngredients,
+    getRecipeById
 };
